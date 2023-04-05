ae3f1c2eed5dafd4333c9b58ccfe1b541a1072fe
Candice <candice.martens@hotmail.com>
2023-04-05 18:45:27 +0200

    moving to center

--- hover.js
+++ hover.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
   $("#project1").on("mouseenter", function () {
     $("#hover2").hide();
-    $("#hover3").hide();
-    $("#hover4").hide();
-    $("#hover5").hide();
+    // $("#hover3").hide();
+    // $("#hover4").hide();
+    // $("#hover5").hide();
     $("#hover1").show(1);
   });
 
   $("#project2").on("mouseenter", function () {
     $("#hover1").hide();
-    $("#hover3").hide();
-    $("#hover4").hide();
-    $("#hover5").hide();
+    // $("#hover3").hide();
+    // $("#hover4").hide();
+    // $("#hover5").hide();
     $("#hover2").show(1);
   });
 
-  $("#project3").on("mouseenter", function () {
-    $("#hover1").hide();
-    $("#hover2").hide();
-    $("#hover4").hide();
-    $("#hover5").hide();
-    $("#hover3").show(1);
-  });
+  // $("#project3").on("mouseenter", function () {
+  //   $("#hover1").hide();
+  //   $("#hover2").hide();
+  //   $("#hover4").hide();
+  //   $("#hover5").hide();
+  //   $("#hover3").show(1);
+  // });
 
-  $("#project4").on("mouseenter", function () {
-    $("#hover1").hide();
-    $("#hover2").hide();
-    $("#hover3").hide();
-    $("#hover5").hide();
-    $("#hover4").show(1);
-  });
+  // $("#project4").on("mouseenter", function () {
+  //   $("#hover1").hide();
+  //   $("#hover2").hide();
+  //   $("#hover3").hide();
+  //   $("#hover5").hide();
+  //   $("#hover4").show(1);
+  // });
 
-  $("#project5").on("mouseenter", function () {
-    $("#hover1").hide();
-    $("#hover2").hide();
-    $("#hover3").hide();
-    $("#hover4").hide();
-    $("#hover5").show(1);
-  });
+  // $("#project5").on("mouseenter", function () {
+  //   $("#hover1").hide();
+  //   $("#hover2").hide();
+  //   $("#hover3").hide();
+  //   $("#hover4").hide();
+  //   $("#hover5").show(1);
+  // });
 });
