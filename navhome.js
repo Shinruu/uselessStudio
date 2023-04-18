dc21f9dfb3ce39cb79aab9d18316b566946747b0
Candice <candice.martens@hotmail.com>
2023-04-18 13:22:55 +0200

    updating javascript files

--- navhome.js
+++ navhome.js
@@ -1,3 +1,18 @@
+// check if the preloader has already been loaded
+if (!localStorage.getItem("preloaderLoaded")) {
+  // show the preloader
+  document.getElementById("preloader").style.display = "block";
+
+  // do something (e.g. load assets)
+  setTimeout(function () {
+    // add a class to trigger the fade out animation
+    document.getElementById("preloader").classList.add("fade-out");
+    // save that the preloader has been loaded
+    localStorage.setItem("preloaderLoaded", true);
+  }, 2400);
+  // 2400 is the timer for the gif itself
+}
+
 // Get the navigation banner elements
 const navBanner = document.querySelectorAll(".nav-link");
 
@@ -32,4 +47,29 @@ if (localStorage.getItem("navigationShown") !== "true") {
     elem.style.display = "block";
     elem.style.opacity = "1";
   });
+}
+
+// Check if the user has accepted cookies
+if (localStorage.getItem("cookiesAccepted") !== "true") {
+  // Check if the banner has been displayed before
+  if (localStorage.getItem("cookieBannerDisplayed") !== "true") {
+    // Set a delay of 2.4 seconds before showing the cookie banner
+    setTimeout(function () {
+      document.getElementById("cookieBanner").style.display = "block";
+      // Store that the banner has been displayed before
+      localStorage.setItem("cookieBannerDisplayed", "true");
+    }, 2400);
+  } else {
+    document.getElementById("cookieBanner").style.display = "block";
+  }
+
+  // Add an event listener to the "Accept" button
+  document
+    .getElementById("acceptCookies")
+    .addEventListener("click", function () {
+      // Store that the user has accepted cookies
+      localStorage.setItem("cookiesAccepted", "true");
+      // Hide the cookie banner
+      document.getElementById("cookieBanner").style.display = "none";
+    });
 }
