ed18244e5bc9b2de80f1fbcdd232f3968e8e178c
Candice <candice.martens@hotmail.com>
2023-04-26 16:26:41 +0200

    paleis het loo

--- hover.js
+++ hover.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#project1").on("mouseenter", function () {
     $("#hover2").hide();
-    // $("#hover3").hide();
+    $("#hover3").hide();
     // $("#hover4").hide();
     // $("#hover5").hide();
     $("#hover1").show(1);
@@ -9,19 +9,19 @@ $(document).ready(function () {
 
   $("#project2").on("mouseenter", function () {
     $("#hover1").hide();
-    // $("#hover3").hide();
+    $("#hover3").hide();
     // $("#hover4").hide();
     // $("#hover5").hide();
     $("#hover2").show(1);
   });
 
-  // $("#project3").on("mouseenter", function () {
-  //   $("#hover1").hide();
-  //   $("#hover2").hide();
-  //   $("#hover4").hide();
-  //   $("#hover5").hide();
-  //   $("#hover3").show(1);
-  // });
+  $("#project3").on("mouseenter", function () {
+    $("#hover1").hide();
+    $("#hover2").hide();
+    // $("#hover4").hide();
+    // $("#hover5").hide();
+    $("#hover3").show(1);
+  });
 
   // $("#project4").on("mouseenter", function () {
   //   $("#hover1").hide();
